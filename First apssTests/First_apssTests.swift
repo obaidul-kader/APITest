8ddad4e55849b201ccff7794ab44dfbb31ce3fb5
U.M. islam <u.m.hamidulislam@labs-mac.local>
2019-12-19 17:05:56 +0600

    Initial Commit

---
+++ First apssTests/First_apssTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_apssTests.swift
+//  First apssTests
+//
+//  Created by U.M. Hamidul islam on 12/19/19.
+//  Copyright © 2019 U.M. Hamidul islam. All rights reserved.
+//
+
+import XCTest
+@testable import First_apss
+
+class First_apssTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
